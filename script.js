5641134d66926ae4358315a7d264df450f23cc04
arturhimawari <artur.priynka@escola.pr.gov.br>
2025-10-28 08:18:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click',fuction(){
         const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' this.getAttribute(data-proximo);
+
+        atual.classlist.remove('ativo');
+        document.getElementById(proximoPasso).classlist.add('ativo');
+
     })
 })
